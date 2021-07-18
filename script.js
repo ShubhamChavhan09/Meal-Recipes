46aab56f939824d6963f81715a426b8d4b76691d
Shubham Chavhan <shubh.chavhan00@gmail.com>
2021-07-19 03:01:51 +0530

    added fetch api and function to show meal images and name

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-const search = document.getElementById("search"),
-  submit = document.getElementById("submit"),
-  random = document.getElementById("random"),
-  mealsEl = document.getElementById("meals"),
-  resultHeading = document.getElementById("result-heading"),
-  single_mealEl = document.getElementById("single-meal");
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEl = document.getElementById("single-meal");
+
+function searchMeal(e) {
+  e.preventDefault();
+
+  //clear
+  single_mealEl.innerHTML = "";
+
+  // get the search input
+  const term = search.value;
+
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}' :</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There is no search results. Try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+                    <div class='meal'>
+                        <img src='${meal.strMealThumb}' alt='${meal.strMeal}' />
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                   </div>`
+            )
+            .join("");
+        }
+      });
+    //clear search text
+    search.value = "";
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+submit.addEventListener("submit", searchMeal);
