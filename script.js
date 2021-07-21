16e296399b1a830fd60a8d1d5c753d91cb4aec03
Shubham Chavhan <shubh.chavhan00@gmail.com>
2021-07-22 01:30:34 +0530

    added function and eventlistner to display ingredients and receipe of the meal

--- script.js
+++ script.js
@@ -44,6 +44,47 @@ function searchMeal(e) {
   }
 }
 
+//fetch meal by id
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+//add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+  <h1>${meal.strMeal}</h1>
+  <img src="${meal.strMealThumb}" alt="${meal.strMeal}/>"
+  <div class="single-meal-info">
+    ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+    ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+    </div>
+    <div class="main">
+    <p>${meal.strInstructions}</p>
+    <h2>Ingredients</h2>
+    <ul>
+    ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+    </ul>
+  </div>
+  `;
+}
+
 submit.addEventListener("submit", searchMeal);
 
 mealsEl.addEventListener("click", (e) => {
@@ -54,5 +95,8 @@ mealsEl.addEventListener("click", (e) => {
       return false;
     }
   });
-  console.log(mealInfo);
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
 });
